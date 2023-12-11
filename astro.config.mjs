b85b4c80f9e36ebae949284941fa635d05c912f8
PaulPHPE <93597135+PaulPHPE@users.noreply.github.com>
2023-12-11 16:06:59 +0100

    feat(frontend): sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 import { defineConfig } from 'astro/config';
 import nodejs from '@astrojs/node';
 import solid from '@astrojs/solid-js';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid()],
+  integrations: [solid(), sitemap({
+    filter: (page) =>
+      page !== 'https://gestalt.peharpre.fr/404/' &&
+      page !== 'https://gestalt.peharpre.fr/mention-legales/'
+  })],
   compressHTML: true,
   output: 'server',
   adapter: nodejs({
     mode: 'standalone',
   }),
+  site: 'https://gestalt.peharpre.fr/',
 });
