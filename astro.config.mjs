d09f6827eb4748b2715e1dcc191db9bfa715dc2c
PaulPHPE <93597135+PaulPHPE@users.noreply.github.com>
2023-12-20 21:06:35 +0100

    update netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 import sitemap from '@astrojs/sitemap';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
