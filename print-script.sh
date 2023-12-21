5eb0a4b4f9cc1b201d59230c2e23035b15ea845e
PaulPHPE <93597135+PaulPHPE@users.noreply.github.com>
2023-12-21 12:26:47 +0100

    fix: mac case sensitivity with git

--- print-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Print the current directory
-echo "chocolat"
-echo "Current Directory: $(pwd)"
-
-# Print the contents of the current directory
-echo -e "\nContents of Current Directory:"
-ls
