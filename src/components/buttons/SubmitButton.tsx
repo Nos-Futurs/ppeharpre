b878593ffd47d1c24fba8597b9c131088964881e
PaulPHPE <93597135+PaulPHPE@users.noreply.github.com>
2024-02-12 11:43:27 +0100

    fix(email): sent

--- src/components/buttons/SubmitButton.tsx
+++ src/components/buttons/SubmitButton.tsx
@@ -1,20 +1,22 @@
-import "./buttons.scss";
+import { Component } from 'solid-js';
+import './buttons.scss';
 
 interface SubmitButtonProps {
-    name: string; 
-    onClick: () => void
+  name: string;
+  onClick: () => void;
 }
 
-const SubmitButton = (props: SubmitButtonProps) => {  
+const SubmitButton: Component<SubmitButtonProps> = (props: SubmitButtonProps) => {
   return (
-        <button
-          class="submitButton"
-          onclick={props.onClick}
-        >
-          {props.name}
-        </button>
-        
-  )
-}
+    <button
+      class="submitButton"
+      onClick={(): void => {
+        props.onClick();
+      }}
+    >
+      {props.name}
+    </button>
+  );
+};
 
 export default SubmitButton;
